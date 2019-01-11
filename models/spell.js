86b600722d664c336e4b74738f6bc0fc621abf6d
Nolan Kovacik <noltron000@gmail.com>
2019-01-10 19:10:11 -0800

    🏙 cleaning mongoose model

--- models/spell.js
+++ models/spell.js
@@ -3,129 +3,58 @@ const mongoose = require('mongoose');
 // Set Schema
 const Schema = mongoose.Schema;
 const SpellSchema = new Schema({
-	////////////////////////
 	// Spell Heading Data //
-	////////////////////////
 	'name': {
-		// Only letters, numbers, spaces, hyphens, apostrophes, and slashes.
 		type: String,
 		required: true,
 		unique: true
 	},
 	'slug': {
-		// Copy of name, using only letters and hyphens.
 		type: String,
 		required: true,
 		unique: true
 	},
-	'level': {
-		// only single-digit numbers including 0-9 are permitted.
-		// 0 value is considered a cantrip.
-		type: Number,
-	},
-	'school': {
-		// Only one of the 8 schools are permitted:
-		// - abjuration
-		// - conjuration
-		// - divination
-		// - enchantment
-		// - evocation
-		// - illusion
-		// - necromancy
-		// - transmutation
-		type: String,
-	},
-	///////////////////////////////
+	'level': Number,
+	'school': String,
+
 	// Casting Time + Spell Area //
-	///////////////////////////////
-	'cast-time': {
-		type: String,
-	},
-	'cast-trigger': {
-		type: String
-	},
-	'duration': {
-		type: String,
-	},
-	'dur-trigger': {
-		type: String
-	},
-	'num-targets': {
-		type: String
-	},
-	'range': {
-		type: String
-	},
+	'cast-time': String,
+	'cast-trigger': String,
+	'duration': String,
+	'dur-trigger': String,
+	'num-targets': String,
+	'range': String,
 	'area': {
-		'shape': {
-			type: String
-		},
-		'radius': {
-			type: String
-		},
-		'length': {
-			type: String
-		},
-		'width': {
-			type: String
-		},
-		'height': {
-			type: String
-		}
+		'shape': String,
+		'radius': String,
+		'length': String,
+		'width': String,
+		'height': String
 	},
-	///////////////////////
+
 	// Tags + Components //
-	///////////////////////
-	'tags': [{
-		type: String
-	}],
+	'tags': [String],
 	'components': {
-		'verbal': {
-			type: String
-		},
-		'somatic': {
-			type: String
-		},
-		'material': {
-			type: String
-		}
+		'verbal': String,
+		'somatic': String,
+		'material': String
 	},
-	/////////////////
+
 	// Description //
-	/////////////////
-	'description': {
-		type: String
-	},
-	'desc-higher': {
-		type: String
-	},
-	///////////////////
+	'description': String,
+	'desc-higher': String,
 	// Compatibility //
-	///////////////////
-	'classes': [{
-		type: String
-	}],
-	'subclasses': [{
-		type: String
-	}],
-	'races': [{
-		type: String
-	}],
-	'subraces': [{
-		type: String
-	}],
-	/////////////////////////
-	// Class Compatibility //
-	/////////////////////////
-	'parent-src': { // parent source for index library
+	'classes': [String],
+	'subclasses': [String],
+	'races': [String],
+	'subraces': [String],
+	'parent-src': [{ // parent source for index library
 		'source': {
 			type: Schema.Types.ObjectId,
 			ref: 'Source'
 		},
-		'page': {
-			type: String
-		}
-	},
+		'page': String
+	}],
 	'sibling-ref': { // sibling reference for spell diffs
 		type: Schema.Types.ObjectId,
 		ref: 'Spell'
