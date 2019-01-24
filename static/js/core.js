944431782c30e863feb003901d36af8be95e43d7
Nolan Kovacik <noltron000@gmail.com>
2019-01-24 11:35:01 -0800

    📼 section 2: watching changes happen before my eyes

---
+++ static/js/core.js
@@ -0,0 +1,38 @@
+
+const changeFormTab = () => {
+	const formTabs = document.getElementsByName('form-tab');
+	const sections = document.getElementsByTagName('section');
+	for (const eachTab of formTabs) {
+		if (eachTab.checked === true) {
+			for (const eachSec of sections) {
+				if (eachTab.value === eachSec.id) {
+					console.log(eachSec);
+					eachSec.removeAttribute('hidden');
+				} else {
+					eachSec.setAttribute('hidden', '');
+				}
+			}
+		}
+	}
+}
+
+const updateSlug = () => {
+	const name = document.getElementById('name');
+	const slug = document.getElementById('slug');
+	const output = name.value
+		.toString()                     // Just in case
+		.toLowerCase()                  // Ban the caps
+		.replace(/\s+/g, '-')           // Replace spaces with -
+		.replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+		.replace(/\-\-+/g, '-')         // Replace multiple - with single -
+		.replace(/^-+/, '')             // Trim - from start of text
+		.replace(/-+$/, '');            // Trim - from end of text
+	slug.value = output
+}
+
+const updateCastTimeNum = () => {
+
+}
+
+changeFormTab()
+updateSlug()
