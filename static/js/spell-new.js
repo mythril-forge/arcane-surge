199787e1dd67f7561891c06ca40e734f014a6150
Nolan Kovacik <noltron000@gmail.com>
2019-02-10 15:11:33 -1000

    💡 basic source ±add/del now...functional

--- static/js/spell-new.js
+++ static/js/spell-new.js
@@ -1,35 +1,53 @@
 let citationIndex = 0;
 
 // runs when [+] gets pressed.
-const createCitation = (buttonID) => {
+const createCitation = (buttonID, SourceDict) => {
 	citationIndex += 1; // increment citationIndex to get a unique ID for the new element
 	// Adds an element to the document
 	const button = document.getElementById(buttonID);
-	const parent = button.parentNode; // declaring fieldset
+	const parent = button.parentNode.parentNode; // declaring fieldset
 	const newDiv = document.createElement('div');
-	// Really need to implement something better. React?
-	const html = `
-	<button type='button' id='citations-` + citationIndex + `-delete' onclick='deleteCitation(this.id)'>
-		<code>&minus;</code>
-	</button>
 
-	<select name='citations.` + citationIndex + `.source' id='citations-` + citationIndex + `-source'>
-		<optgroup label='select a source:'>
-			<option disabled hidden selected />
-			{{#each EverySource}}
-			<option label='{{this.name}}' value='{{this._id}}' />
-			{{/each}}
-		</optgroup>
-	</select>
+	let options = ''
+	for (sourceID in SourceDict) {
+		options += `<option label="${SourceDict[sourceID]}" value="${sourceID}" />\n`
+	}
 
-	<input type='number' name='citations.` + citationIndex + `.page' id='citations-` + citationIndex + `-page' min='0' max='999999' placeholder='page' />`
-	newDiv.setAttribute('id', 'citations-' + citationIndex);
+	const html =
+		`
+		<button type='button' id='citations-${citationIndex}-delete' onclick='deleteCitation(this.id)'>
+			<code>&minus;</code>
+		</button>
+
+		<select name='citations.${citationIndex}.source' id='citations-${citationIndex}-source'>
+			<optgroup label='select a source:'>
+				<option disabled hidden selected />
+				${options}
+			</optgroup>
+		</select>
+
+		<input type='number' name='citations.${citationIndex}.page' id='citations-${citationIndex}-page' min='0' max='999999' placeholder='page' />
+		`
+
+	newDiv.setAttribute('id', `citations-${citationIndex}`);
 	newDiv.innerHTML = html;
 	parent.appendChild(newDiv);
+	/* === THIS PIECE MAY BE A BETTER ALTERNATIVE ===
+		const test = ({ optionsArray, citationIndex }) => {
+			return (`
+		<select name='citations. ${citationIndex} .source' id='citations-` + citationIndex + `-source'>
+			<optgroup label='select a source:'>
+				<option disabled hidden selected />
+				${optionsArray.map((option) => `<option value="${option.value}">${option.name}</option>`).concat()}
+			</optgroup>
+		</select>
+	*/
 }
+
 
 // runs when [-] gets pressed.
 const deleteCitation = (buttonID) => {
+	citationIndex -= 1;
 	const button = document.getElementById(buttonID);
 	const parent = button.parentNode; // declaring fieldset
 	parent.parentNode.removeChild(parent); // removing self
